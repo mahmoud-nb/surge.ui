521693cb5636399ddf8d29fb7e53ffb7d0eacf06
Mahmoud <mahmoud.nb@gmail.com>
2025-11-10 16:16:02 +0100

    feat: update package.json and vite.config.ts for project metadata and library name correction

--- package/vite.config.ts
+++ package/vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'SurgeUpDS',
+      name: 'SurgeUI',
       formats: ['es', 'umd'],
       fileName: (format) => `index.${format}.js`
     },
